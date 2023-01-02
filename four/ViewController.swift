6efd78c43e00c62f595df066ff5c5d990f36b408
Ostap Artym <85745668+artym125@users.noreply.github.com>
2023-01-03 00:12:25 +0200

    Initial Commit

---
+++ four/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  four
+//
+//  Created by Ostap Artym on 03.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
